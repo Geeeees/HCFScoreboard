73f947b917d48473ddeaf73c2837373e871a97a5
Geski <Geski@users.noreply.github.com>
2015-11-29 03:35:18 -0300

    New spawn tag time

--- src/com/fuzzycraft/fuzzy/constants/Defaults.java
+++ src/com/fuzzycraft/fuzzy/constants/Defaults.java
@@ -10,6 +10,6 @@ public class Defaults {
 	public static final String TITLE = "&aHCF &c[Map 1]";
 	public static final String SPAWN_TAG_TITLE = "&4SpawnTag: &a";
 	public static final String PEARL_COOLDOWN_TITLE = "&dPearlCD: &a";
-	public static final int SPAWN_TAG_MAX_TIME = 60;
+	public static final int SPAWN_TAG_MAX_TIME = 30;
 	public static final int PEARL_COOLDOWN_MAX_TIME = 16;
 }

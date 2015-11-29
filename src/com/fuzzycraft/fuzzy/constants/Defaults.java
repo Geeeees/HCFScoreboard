f629f9bef5573a62dacdfe82b3c25900eb2d1ea0
Geski <Geski@users.noreply.github.com>
2015-11-29 03:40:17 -0300

    Ups

--- src/com/fuzzycraft/fuzzy/constants/Defaults.java
+++ src/com/fuzzycraft/fuzzy/constants/Defaults.java
@@ -8,8 +8,9 @@ package com.fuzzycraft.fuzzy.constants;
 
 public class Defaults {
 	public static final String TITLE = "&aHCF &c[Map 1]";
-	public static final String SPAWN_TAG_TITLE = "&4SpawnTag: &a";
-	public static final String PEARL_COOLDOWN_TITLE = "&dPearlCD: &a";
+	public static final String SPAWN_TAG_TITLE = "&4SpawnTag: &f";
+	public static final String PEARL_COOLDOWN_TITLE = "&dEnderpearl Cooldown: &f";
+	public static final String PVP_TIMER_TIME = "&cPvp timer: &f"
 	public static final int SPAWN_TAG_MAX_TIME = 30;
 	public static final int PEARL_COOLDOWN_MAX_TIME = 16;
 	public static final int PVP_TIMER_MAX_TIME = 1200;
